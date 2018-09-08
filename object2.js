443ade230579d23dd5558a713ce9574e341a52a0
AsifurRahman <asifur355@gmail.com>
2018-09-08 06:50:36 +0600

    2nd commit

---
+++ object2.js
@@ -0,0 +1,18 @@
+
+
+let objectTest=[{
+    name:"Asif",
+    roll:"Coder",
+    code:"2"
+},{
+    name:"Horin",
+    roll:"SQA",
+    code:"1",
+},{
+    name:"Arif",
+    roll:"Programmer",
+    code:"3",
+}]
+
+
+console.log(objectTest)
